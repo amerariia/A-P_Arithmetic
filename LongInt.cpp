8a590bf25eaf6ff86b9638dc13f9eac7430cdde8
sergivna <34794920+sergivna@users.noreply.github.com>
2019-10-22 22:30:47 +0300

    fhj

--- LongInt.cpp
+++ LongInt.cpp
@@ -1,26 +1,5 @@
 #include "LongInt.h"
 
-LongInt::LongInt(string& number)
-{
-	length = number.size();
-	int i = 0;
-	if (number[i] == '-')
-	{
-		isPosit = false;
-		length--;
-		i++;
-	}
-
-	num = new int[length];
-
-	for (int j = 0; j < length; ++j)
-	{
-		num[j] = number[i] - '0';
-		i++;
-	}
-	
-}
-
 LongInt& LongInt::operator+ (LongInt& num2)
 {
 	return *this;
