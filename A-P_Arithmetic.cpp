b15909d503a8a7533e66b51bd688ce5f7f3e91af
Natalia Sokol <kvitnats@gmail.com>
2019-10-22 21:06:27 +0300

    Добавьте файлы проекта.

---
+++ A-P_Arithmetic.cpp
@@ -0,0 +1,12 @@
+﻿// A-P_Arithmetic.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+}
